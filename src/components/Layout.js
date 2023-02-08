10927ff5b8984f967774459b73ec54de19945050
divyabaid16 <divyabaid16@gmail.com>
2023-02-08 15:19:44 +0400

    Movie API Frontend ReactJS

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+//rafce  and tab
+import React from 'react'
+
+const Layout = () => {
+  return (
+   <main>
+        <Outlet/>
+   </main>
+  );
+}
+
+export default Layout
