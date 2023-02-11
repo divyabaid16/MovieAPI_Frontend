9ebca1120ce405534e476e686f7391523f119c7d
divyabaid16 <divyabaid16@gmail.com>
2023-02-11 16:41:36 +0400

    Updating backend URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'https://movieapi-backend.herokuapp.com',
     headers: {'Access-Control-Allow-Origin': '*'}
     // headers: {"ngrok-skip-browser-warning": "true"} //CORS resolution
 });
